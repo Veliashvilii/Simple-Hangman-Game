b7f3734576b9e9aaf14e7da2ea80082634c771bd
Metehan Belli <metehanbelli8@gmail.com>
2024-05-14 13:09:17 +0300

    Initial Commit

---
+++ Simple Hangman Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Hangman Game
+//
+//  Created by Metehan Veliashvili on 14.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
